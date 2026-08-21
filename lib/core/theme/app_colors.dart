7830062b86c3e4b2676cf90376a0eeb5d6833e49
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2026-08-21 16:34:03 +0300

    Add AppColors palette for light and dark themes

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color primaryNeonLime = Color(0xFF70E000);
+  static const Color primaryLimeDark = Color(0xFF38B000);
+  static const Color secondaryAmber = Color(0xFFFF9F1C);
+  static const Color accentCyan = Color(0xFF00E5FF);
+
+  static const Color backgroundDark = Color(0xFF0B1015);
+  static const Color cardDark = Color(0xFF141C24);
+  static const Color cardDarkElevated = Color(0xFF1B2631);
+  static const Color inputBorderDark = Color(0xFF22303C);
+  static const Color textMainDark = Color(0xFFF1F5F9);
+  static const Color textSecondaryDark = Color(0xFF94A3B8);
+  static const Color textMutedDark = Color(0xFF64748B);
+
+  static const Color backgroundLight = Color(0xFFF8FAFC);
+  static const Color cardLight = Color(0xFFFFFFFF);
+  static const Color cardLightBorder = Color(0xFFE2E8F0);
+  static const Color textMainLight = Color(0xFF0F172A);
+  static const Color textSecondaryLight = Color(0xFF475569);
+  static const Color textMutedLight = Color(0xFF94A3B8);
+
+  static const Color success = Color(0xFF22C55E);
+  static const Color error = Color(0xFFEF4444);
+  static const Color warning = Color(0xFFF59E0B);
+}
