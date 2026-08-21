e73019d710ff643860498d22f6fe7ff4a85a07c7
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2026-08-21 16:41:39 +0300

    Add modern linear gradients for buttons, cards, and scanner

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  
+
   static const Color primaryNeonLime = Color(0xFF70E000);
   static const Color primaryLimeDark = Color(0xFF38B000);
   static const Color secondaryAmber = Color(0xFFFF9F1C);
@@ -26,4 +26,28 @@ class AppColors {
   static const Color success = Color(0xFF22C55E);
   static const Color error = Color(0xFFEF4444);
   static const Color warning = Color(0xFFF59E0B);
+
+  static const LinearGradient primaryLimeGradient = LinearGradient(
+    colors: [primaryNeonLime, primaryLimeDark],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient fireCalorieGradient = LinearGradient(
+    colors: [Color(0xFFFF9F1C), Color(0xFFFF4800)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient darkCardGradient = LinearGradient(
+    colors: [Color(0xFF1A232E), Color(0xFF10171E)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient scannerGlassGradient = LinearGradient(
+    colors: [Color(0x3370E000), Color(0x0070E000)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
 }
