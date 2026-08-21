3cf829f47c045ac75a5534e5d75ee53b79a5a65d
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2026-08-21 16:43:26 +0300

    Add local storage key constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String isOnboardingSeen = 'is_onboarding_seen';
+  static const String isGuestUser = 'is_guest_user';
+  static const String cachedUserToken = 'cached_user_token';
+  static const String appThemeMode = 'app_theme_mode';
+}
